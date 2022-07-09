bd411e47afab78c9bbf942090cd4fd3e19684ec3
Plamen Simeonov <61594367+haibusa1991@users.noreply.github.com>
2022-07-09 15:18:10 +0300

    Delete src directory

--- src/lecture03/ME09NumbersFromOneToTen.java
+++
@@ -1,9 +0,0 @@
-package lecture03;
-
-public class ME09NumbersFromOneToTen {
-    public static void main(String[] args) {
-        for (int i = 1; i < 11; i++) {
-            System.out.println(i);
-        }
-    }
-}

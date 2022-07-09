c5bfc886fbebd644199ccbf8dcf68a747c4a15be
Plamen Simeonov <61594367+haibusa1991@users.noreply.github.com>
2022-07-09 15:17:55 +0300

    Delete Java-basics directory

--- Java-basics/src/lecture04/E01NumbersEndingInSeven.java
+++
@@ -1,9 +0,0 @@
-package lecture04;
-
-public class E01NumbersEndingInSeven {
-    public static void main(String[] args) {
-        for (int i = 7; i < 1000; i+=10) {
-            System.out.println(i);
-        }
-    }
-}
